3b1d7e2b14acb50e986f1682306bff99c1952629
taoyuchuan <taoyuchuan@gmail.com>
2021-01-16 15:39:41 -0500

    Add event handling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
 import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FunctionClick />
+        <ClassClick />
         <Counter />
         <Message />
         <Greet name = "Bruce" heroName = "Batman">
