2bcc3610ccd9270103556096386f1d8c88e89899
taoyuchuan <taoyuchuan@gmail.com>
2021-01-17 14:34:26 -0500

    Add key in list

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList() {
+  /*
   const persons = [
     {
       id: 1,
@@ -22,10 +23,15 @@ function NameList() {
       skill: 'JavaScript'
     },
   ]
-  const personList = persons.map(person => (<Person person = {person} />))
+  const personList = persons.map(person => (<Person key = { person.id } person = {person} />))
   return(
     <div>{ personList }</div>
   )
+  */
+
+  const names = ['Adam', 'Black', 'Clark', 'Adam']
+  const nameList = names.map((name, index) => <h2 key = { index }>{ index } { name }</h2>)
+  return <div>{ nameList }</div>
 }
 
 export default NameList
