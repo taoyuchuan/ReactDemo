08490dded0a636231e669f5285d7ea62a0170c7e
taoyuchuan <taoyuchuan@gmail.com>
2021-01-18 14:01:37 -0500

    Add code to check the order of component lifecycle

--- src/App.js
+++ src/App.js
@@ -17,11 +17,13 @@ import Inline from './components/Inline'
 import './appStyles.css'
 import  styles from './appStyles.module.css'
 import Form from './components/Form'
+import LifecycleA from './components/LifecycleA'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LifecycleA />
         <Form />
         <h1 className = 'error'>Error</h1>
         <h1 className = { styles.success }>Success</h1>
