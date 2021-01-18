517f79326a37ec77a6676c3dcd75ddb85b61f7ef
taoyuchuan <taoyuchuan@gmail.com>
2021-01-17 19:50:43 -0500

    Add form elements

--- src/App.js
+++ src/App.js
@@ -16,11 +16,13 @@ import Stylesheet from './components/Stylesheet'
 import Inline from './components/Inline'
 import './appStyles.css'
 import  styles from './appStyles.module.css'
+import Form from './components/Form'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Form />
         <h1 className = 'error'>Error</h1>
         <h1 className = { styles.success }>Success</h1>
         <Inline />
