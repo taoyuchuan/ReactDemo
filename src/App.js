b7360445c8e65c9752c9ac961f2cb8dab6049dd2
taoyuchuan <taoyuchuan@gmail.com>
2021-01-19 19:43:18 -0500

    Add refs with class component

--- src/App.js
+++ src/App.js
@@ -22,11 +22,13 @@ import FragmentDemo from './components/FragmentDemo'
 import Table from './components/Table'
 import ParentComp from './components/ParentComp'
 import RefsDemo from './components/RefsDemo'
+import FocusInput from './components/FocusInput'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FocusInput />
         <RefsDemo />
         <ParentComp />
         <Table />
