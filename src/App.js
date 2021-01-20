d80464c40695632ce99759148b0dc29103850a6c
taoyuchuan <taoyuchuan@gmail.com>
2021-01-19 20:01:05 -0500

    Add ref forwarding

--- src/App.js
+++ src/App.js
@@ -23,11 +23,13 @@ import Table from './components/Table'
 import ParentComp from './components/ParentComp'
 import RefsDemo from './components/RefsDemo'
 import FocusInput from './components/FocusInput'
+import FRParentInput from './components/FRParentInput'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <FRParentInput />
         <FocusInput />
         <RefsDemo />
         <ParentComp />
