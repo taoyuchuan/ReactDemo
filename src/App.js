5653a57d9918e6b4c65d24afde44aebbcbf2b933
taoyuchuan <taoyuchuan@gmail.com>
2021-01-20 19:48:08 -0500

    Add Error Boundary

--- src/App.js
+++ src/App.js
@@ -25,11 +25,22 @@ import RefsDemo from './components/RefsDemo'
 import FocusInput from './components/FocusInput'
 import FRParentInput from './components/FRParentInput'
 import PortalDemo from './components/PortalDemo'
+import Hero from './components/Hero'
+import ErrorBoundary from './components/ErrorBoundary'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <ErrorBoundary>
+          <Hero heroName = 'Batman' />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName = 'Superman' />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName = 'Joker' />
+        </ErrorBoundary>
         <PortalDemo />
         <FRParentInput />
         <FocusInput />
